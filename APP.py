1b41f34b4f2ec373b278ceadd088683f3bfca33a
happyakki <greenstepankit@gmail.com>
2024-09-24 00:07:41 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,8 +1,8 @@
-def addition(a,b,c):
-    return a+b+c
+def addition(a,b,c,d):
+    return a+b+c+d
 
-def sub(a,b):
-    return a-b
+def sub(a,b,c):
+    return a-b-c
 
 def division(a,b):
     return a/b
